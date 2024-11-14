fd03626bfde66fd2ec7edfa9280c13aafa2a219d
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-14 19:51:07 +0100

    creacion de pagina main, añadidas imagenes y procesadores de estilos y javascript

---
+++ Reto 1º Evaluacion/js/main.js
@@ -0,0 +1,9 @@
+function changeMainImage(imageSrc) {
+    document.getElementById("mainImage").src = imageSrc;
+}
+
+// Cambia la imagen principal y el texto del segundo carrusel
+function changeTextCarouselImage(imageSrc, text) {
+    document.getElementById("textCarouselMainImage").src = imageSrc;
+    document.getElementById("carouselText").textContent = text;
+}
