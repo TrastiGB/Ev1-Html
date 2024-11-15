1cc60016370acc79829583572179a0050c89f4c5
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-11-15 16:55:36 +0100

    correccion de header footer y carroussell

--- Reto 1º Evaluacion/js/main.js
+++ Reto 1º Evaluacion/js/main.js
@@ -1,9 +1,53 @@
-function ImagenCarousel1(imageSrc) {
-    document.getElementById("mainImage").src = imageSrc;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    let currentSlide = 0;
+    const images = [
+        '../img/Como_entrenar_dragon_3_E.png',
+        '../img/robot_salvaje.jpg',
+        '../img/smile2png.png',
+        '../img/terrifier-3-4245154.png',
+        '../img/Venom-The-Last-Dance.png'
+    ];
+    
+    const mainImage = document.querySelector('.carousel__image');
+    const thumbnails = document.querySelectorAll('.carousel__thumbnail');
+    const arrowLeft = document.querySelector('.carousel__arrow--left');
+    const arrowRight = document.querySelector('.carousel__arrow--right');
 
-// Cambia la imagen principal y el texto del segundo carrusel
-function changeTextCarouselImage(imageSrc, text) {
-    document.getElementById("textCarouselMainImage").src = imageSrc;
-    document.getElementById("carouselText").textContent = text;
-}
+    // Mostrar la imagen actual
+    function showSlide(index) {
+        currentSlide = index;
+        mainImage.src = images[currentSlide];
+        updateActiveThumbnail();
+    }
+
+    // Ir al siguiente slide
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % images.length;
+        showSlide(currentSlide);
+    }
+
+    // Ir al slide anterior
+    function previousSlide() {
+        currentSlide = (currentSlide - 1 + images.length) % images.length;
+        showSlide(currentSlide);
+    }
+
+    // Actualizar la clase activa en la miniatura
+    function updateActiveThumbnail() {
+        thumbnails.forEach((thumb, index) => {
+            thumb.classList.toggle('active', index === currentSlide);
+        });
+    }
+
+    // Eventos para las flechas
+    arrowLeft.addEventListener("click", previousSlide);
+    arrowRight.addEventListener("click", nextSlide);
+
+    // Evento para cada miniatura
+    thumbnails.forEach((thumbnail, index) => {
+        thumbnail.addEventListener("click", () => showSlide(index));
+    });
+
+    // Iniciar mostrando la primera imagen y la miniatura activa
+    showSlide(currentSlide);
+});
